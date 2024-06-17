fa311162268c94d491fbe64a889fb8faf692b94b
Teodora Taleska <taleska000@gmail.com>
2024-06-17 22:14:20 +0200

    Fix: r2 nan to real number

--- scripts/cross_validation.py
+++ scripts/cross_validation.py
@@ -8,10 +8,10 @@ import numpy as np
 def loocv(X, y, model):
     loo = LeaveOneOut()
     mse_loocv = []
-    r2_loocv = []
     mae_loocv = []
     rmse_loocv = []
     predictions = []
+    testing = []
 
     for train_index, test_index in loo.split(X):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
@@ -20,50 +20,37 @@ def loocv(X, y, model):
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
         predictions.append(y_pred[0])
+        testing.append(y_test.values[0])
 
         # Calculate evaluation metrics
         mse, rmse, mae, r2 = calculate_metrics(y_test, y_pred)
-        r2_loocv.append(r2)
         mse_loocv.append(mse)
         mae_loocv.append(mae)
         rmse_loocv.append(rmse)
 
-    avg_r2_loocv = np.mean(r2_loocv)
     avg_mse_loocv = np.mean(mse_loocv)
     avg_mae_loocv = np.mean(mae_loocv)
     avg_rmse_loocv = np.sqrt(avg_mse_loocv)
+    r_squared_loocv = r2_score(predictions, testing)
 
-    print("Average R^2 (LOOCV):", avg_r2_loocv)
+    print("R squared (LOOCV):", r_squared_loocv)
     print("Average MSE (LOOCV):", avg_mse_loocv)
     print("Average RMSE (LOOCV):", avg_rmse_loocv)
     print("Average MAE (LOOCV):", avg_mae_loocv)
 
-    return predictions
-
 
 def k_fold_cv(X, y, model, cv=6):
-
     scoring = {
         'mse': make_scorer(mean_squared_error),
         'mae': make_scorer(mean_absolute_error),
-        'r2': make_scorer(calculate_r2),
+        'r2': make_scorer(r2_score),
     }
 
     cv_results = cross_validate(model, X, y, cv=cv, scoring=scoring)
 
-    scorers = {
-        'mse': 'mean squared error',
-        'mae': 'mean absolute error',
-        'r2': 'r squared',
-    }
-
-    mse = 0
-    for scorer_name, scorer in scorers.items():
-        scores = cv_results['test_' + scorer_name]
-        mean_score = np.mean(scores)
-        if scorer_name == 'mse':
-            mse = mean_score
+    for scorer_name in scoring.keys():
+        mean_score = np.mean(cv_results['test_' + scorer_name])
         print(f"Mean {scorer_name.upper()}: {mean_score}")
-    print('RMSE', np.sqrt(mse))
 
+    print('RMSE', np.sqrt(np.mean(cv_results['test_mse'])))
 
